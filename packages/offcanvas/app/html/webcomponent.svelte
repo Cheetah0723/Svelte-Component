de4b4925fe5fc36384119ffdbd8a7a1c970fd61d
Cheetah0723 <kimuraa.hideyo@gmail.com>
2021-09-04 03:02:55 +0200

    wip on offcanvas

--- packages/offcanvas/app/html/webcomponent.svelte
+++ packages/offcanvas/app/html/webcomponent.svelte
@@ -39,7 +39,15 @@
 	>
 		<div class="offcanvas-header">
 			<h5 class="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
-			<button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close" />
+			<button
+				on:click={() => {
+					opened = false;
+				}}
+				type="button"
+				class="btn-close text-reset"
+				data-bs-dismiss="offcanvas"
+				aria-label="Close"
+			/>
 		</div>
 		<div class="offcanvas-body">
 			<div>Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.</div>
