264b5ed835b09ec6e7542e9f85adaa96c0c243cc
Cheetah0723 <kimuraa.hideyo@gmail.com>
2021-09-05 03:10:22 +0200

    wip on offcanvas

--- packages/offcanvas/app/html/webcomponent.svelte
+++ packages/offcanvas/app/html/webcomponent.svelte
@@ -32,33 +32,61 @@
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@latest/font/bootstrap-icons.css" />
 </svelte:head>
 <div id="webcomponent">
-	<slot name="test"></slot>
+	<slot name="test" />
 	<div
 		class="offcanvas offcanvas-start show"
 		tabindex="-1"
 		aria-labelledby="offcanvasExampleLabel"
 		style="visibility: visible; {opened ? 'transform:none!important' : 'transform:translateX(-100%)!important;'}"
 	>
-		<div class="offcanvas-header">
-			<h5 class="offcanvas-title">Offcanvas</h5>
-			<button
-				on:click={() => {
-					opened = false;
-				}}
-				type="button"
-				class="btn-close text-reset"
-			/>
-		</div>
-		<div class="offcanvas-body">
-			<div>Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.</div>
-			<div class="dropdown mt-3">
-				<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown"> Dropdown button </button>
-				<ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-					<li><a class="dropdown-item" href="#">Action</a></li>
-					<li><a class="dropdown-item" href="#">Another action</a></li>
-					<li><a class="dropdown-item" href="#">Something else here</a></li>
-				</ul>
-			</div>
+		<div class="d-flex flex-column flex-shrink-0 p-3 bg-light" style="min-height:100vh">
+			<h5 class="offcanvas-title" style="margin-bottom:30px">
+				Offcanvas
+
+				<button
+					on:click={() => {
+						opened = false;
+					}}
+					type="button"
+					class="btn-close text-reset"
+					style="float:right"
+				/>
+			</h5>
+
+			<ul class="nav nav-pills flex-column mb-auto">
+				<li class="nav-item">
+					<a href="#" class="nav-link active" aria-current="page">
+						<svg class="bi me-2" width="16" height="16"><use xlink:href="#home" /></svg>
+						Home
+					</a>
+				</li>
+				<li>
+					<a href="#" class="nav-link link-dark">
+						<svg class="bi me-2" width="16" height="16"><use xlink:href="#speedometer2" /></svg>
+						Dashboard
+					</a>
+				</li>
+				<li>
+					<a href="#" class="nav-link link-dark">
+						<svg class="bi me-2" width="16" height="16"><use xlink:href="#table" /></svg>
+						Orders
+					</a>
+				</li>
+				<li>
+					<a href="#" class="nav-link link-dark">
+						<svg class="bi me-2" width="16" height="16"><use xlink:href="#grid" /></svg>
+						Products
+					</a>
+				</li>
+				<li>
+					<a href="#" class="nav-link link-dark">
+						<svg class="bi me-2" width="16" height="16"><use xlink:href="#people-circle" /></svg>
+						Customers
+					</a>
+				</li>
+			</ul>
+			<hr />
+			text
 		</div>
 	</div>
 
