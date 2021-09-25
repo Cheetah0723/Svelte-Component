7cf8d0b11f03365031deba235ac2047fbcfe4a83
Cheetah0723 <kimuraa.hideyo@gmail.com>
2021-09-25 15:47:47 +0200

    WIP

---
+++ packages/formrenderer/app/html/webcomponent.svelte
@@ -0,0 +1,37 @@
+<svelte:options tag="formrenderer-component" />
+
+<script lang="ts">
+	/**
+	 * Svelte Video Gallery WebComponent
+	 * =====================
+	 *
+	 * @contributors: Dario Caruso <dev@dariocaruso.info> (https://dariocaruso.info)
+	 *
+	 * @license: MIT License
+	 *
+	 */
+
+	import { get_current_component } from "svelte/internal";
+	import { createEventDispatcher } from "svelte";
+
+	export let id: string;
+	export let allowdecline: "yes" | "no";
+	let isSet: boolean;
+	$: {
+		if (!id) id = "";
+		isSet = localStorage.getItem(localStorageItem) ? true : false;
+		if (allowdecline !== "yes") allowdecline = "no";
+	}
+	const component = get_current_component();
+	const svelteDispatch = createEventDispatcher();
+
+	const onSubmit = () => {
+		svelteDispatch(name, detail);
+		component.dispatchEvent && component.dispatchEvent(new CustomEvent(name, { detail }));
+	}
+</script>
+
+<style lang="scss">
+	// @import "../styles/bootstrap.scss";
+	@import "../styles/webcomponent.scss";
+</style>
