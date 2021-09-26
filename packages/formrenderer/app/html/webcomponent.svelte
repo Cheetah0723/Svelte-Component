56b2c75bae0660e0403fe8141579437b1fb7aec7
Cheetah0723 <kimuraa.hideyo@gmail.com>
2021-09-26 22:16:13 +0200

    upp

--- packages/formrenderer/app/html/webcomponent.svelte
+++ packages/formrenderer/app/html/webcomponent.svelte
@@ -16,31 +16,31 @@
 	import type { FormSchema } from "@app/types/webcomponent.type";
 	import FormSchemaRenderer from "./FormSchemaRenderer.svelte";
 
-	export let schema: FormSchema | string
-	let _schema: FormSchema = typeof schema === 'string' ? JSON.parse(schema) : schema
+	export let schema: FormSchema | string;
+	let _schema: FormSchema = typeof schema === "string" ? JSON.parse(schema) : schema;
 
 	const component = get_current_component();
 
-	const link = document.createElement('link')
-	link.rel = 'stylesheet'
-	link.href = 'https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css'
+	const link = document.createElement("link");
+	link.rel = "stylesheet";
+	link.href = "https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css";
 
 	component.shadowRoot.prepend(link);
 
 	const svelteDispatch = createEventDispatcher();
 
-	let values: Record<string, string|number|boolean>
-	let isInvalid = true
+	let values: Record<string, string | number | boolean>;
+	let isInvalid = true;
 
 	const onSubmit = () => {
-	  svelteDispatch('submit', values);
-	  component.dispatchEvent?.(new CustomEvent('submit', { detail: values }));
-	}
+		svelteDispatch("submit", values);
+		component.dispatchEvent?.(new CustomEvent("submit", { detail: values }));
+	};
 </script>
 
 {#if _schema}
-  <FormSchemaRenderer schema={_schema} bind:values bind:isInvalid />
-  <button type="button" class="btn btn-primary" disabled={isInvalid} on:click|preventDefault={onSubmit}>Submit</button>	
+	<FormSchemaRenderer schema={_schema} bind:values bind:isInvalid />
+	<button type="button" class="btn btn-primary" disabled={isInvalid} on:click|preventDefault={onSubmit}>Submit</button>
 {/if}
 
 <style lang="scss">
