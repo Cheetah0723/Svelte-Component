2a4d2a1ed5a5d4d7c08563d6c1e81ac4089cf717
Cheetah0723 <kimuraa.hideyo@gmail.com>
2021-11-21 04:47:30 +0100

    better story

--- storybook/stories/FormHost.stories.ts
+++ storybook/stories/FormHost.stories.ts
@@ -64,8 +64,26 @@ const preferences = [
     type: "text",
     placeholder: "Inserisci titolo di default",
     id: "defaultTitle",
+    required: true,
+
     label: "Titolo di default",
     value: "titolotest",
+    params: {
+      minlength: 8,
+      maxlength: 120,
+    },
+  },
+  {
+    type: "number",
+    id: "age",
+    required: true,
+    label: "Age",
+    value: 9,
+    params: {
+      min: 8,
+      max: 120,
+    },
+    validationTip: "Min 8, Max 120",
   },
 ];
 export const FormHostTemplate = Template.bind({});
